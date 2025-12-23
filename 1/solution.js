f9667578ad5576f9b19671ad1001c48dc2139dd4
Udashkins <king_of_hell1@mail.ru>
2025-12-23 19:26:25 +0300

    Update solution.js

--- 1/solution.js
+++ 1/solution.js
@@ -8,5 +8,26 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+export default async function solution() {
+  const sql = postgres(config);
 
+  try {
+    // Создаем таблицу articles
+    await sql`
+      CREATE TABLE articles (
+        title VARCHAR(255),
+        description VARCHAR(255)
+      )
+    `;
+
+    // Добавляем как минимум одну запись в таблицу
+    await sql`
+      INSERT INTO articles (title, description)
+      VALUES ('Первая статья', 'Это описание первой статьи')
+    `;
+
+  } finally {
+    await sql.end();
+  }
+}
 // END
