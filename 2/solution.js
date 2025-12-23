bf686e3cd4cc3962305d3d121be77b32d6fa9438
Udashkins <king_of_hell1@mail.ru>
2025-12-23 19:26:51 +0300

    Update solution.js

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,28 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const solution = async (articles) => {
+  // Подключаемся к базе данных
+  const sql = postgres(config);
+  
+  const ids = [];
 
+  try {
+    for (const article of articles) {
+      const result = await sql`
+        INSERT INTO articles (title, description)
+        VALUES (${article.title}, ${article.description})
+        RETURNING id
+      `;
+      
+      ids.push(result[0].id);
+    }
+    
+    return ids;
+  } finally {
+    await sql.end();
+  }
+};
+
+export default solution;
 // END
